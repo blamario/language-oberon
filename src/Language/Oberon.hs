6b4c23b81e945fc2d83f2f916b0e0ecfd47da809
Mario Blažević <blamario@protonmail.com>
2018-04-04 00:00:44 -0400

    Transitive closure of imports

--- src/Language/Oberon.hs
+++ src/Language/Oberon.hs
@@ -9,6 +9,7 @@ import Data.Functor.Identity (Identity)
 import Data.Functor.Compose (getCompose)
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.Map.Lazy as Map
+import Data.Map.Lazy (Map)
 import Data.Monoid ((<>))
 import Data.Text (Text, unpack)
 import Data.Text.IO (readFile)
@@ -38,14 +39,23 @@ parseNamedModule oberon2 path name =
       getCompose . Grammar.module_prod . parseComplete grammar
          <$> readFile (basePath <> if isDefn then ".Def" else ".Mod")
 
+parseImportsOf :: Bool -> FilePath -> Map Text (Module Ambiguous) -> IO (Map Text (Module Ambiguous))
+parseImportsOf oberon2 path modules =
+   case filter (`Map.notMember` modules) moduleImports
+   of [] -> return modules
+      newImports -> (((modules <>) . Map.fromList . map assertSuccess) <$>
+                     (traverse . traverse) (parseNamedModule oberon2 path) [(p, p) | p <- newImports])
+                    >>= parseImportsOf oberon2 path
+   where moduleImports = foldMap importsOf modules
+         importsOf (Module _ imports _ _ _) = snd <$> imports
+         assertSuccess (m, Left err) = error ("Parse error in module " <> unpack m <> ":" <> show err)
+         assertSuccess (m, Right [p]) = (m, p)
+         assertSuccess (m, Right _) = error ("Ambiguous parses of module " <> unpack m)
+
 parseAndResolveModuleFile :: Bool -> FilePath -> IO (Validation (NonEmpty Resolver.Error) (Module Identity))
 parseAndResolveModuleFile oberon2 path =
-   do Right [rootModule@(Module _ imports _ _ _)] <- parseModule oberon2 <$> readFile path
-      importedModules <- (traverse . traverse) (parseNamedModule oberon2 path) [(p, p) | (_, p) <- imports]
-      let importMap = Map.fromList (assertSuccess <$> importedModules)
-          assertSuccess (m, Left err) = error ("Parse error in module " <> unpack m <> ":" <> show err)
-          assertSuccess (m, Right [p]) = (m, p)
-          assertSuccess (m, Right _) = error ("Ambiguous parses of module " <> unpack m)
-          resolvedImportMap = Resolver.resolveModule predefinedScope resolvedImportMap <$> importMap
+   do Right [rootModule@(Module moduleName imports _ _ _)] <- parseModule oberon2 <$> readFile path
+      importedModules <- parseImportsOf oberon2 path (Map.singleton moduleName rootModule)
+      let resolvedImportMap = Resolver.resolveModule predefinedScope resolvedImportMap <$> importedModules
           predefinedScope = if oberon2 then Resolver.predefined2 else Resolver.predefined
       return $ Resolver.resolveModule predefinedScope resolvedImportMap rootModule
