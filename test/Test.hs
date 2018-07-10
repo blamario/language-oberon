5620dc63ee7cee9083cc4657a820f208d492d149
Mario Blažević <blamario@protonmail.com>
2018-07-09 23:40:48 -0400

    Testing the idempotence of parse&pretty-print

--- test/Test.hs
+++ test/Test.hs
@@ -2,14 +2,26 @@ module Main where
 
 import Data.Either.Validation (Validation(..))
 import Data.List (isSuffixOf)
+import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.Text.IO (readFile)
+import Data.Text.Prettyprint.Doc (Pretty(pretty), layoutPretty, defaultLayoutOptions)
+import Data.Text.Prettyprint.Doc.Render.Text (renderStrict)
 import System.Directory (doesDirectoryExist, listDirectory)
 import System.FilePath.Posix (combine)
+import Text.Grampa (showFailure)
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.HUnit (assertFailure, testCase)
+import Test.Tasty.HUnit (assertFailure, assertEqual, testCase)
 
-import Language.Oberon (parseAndResolveModuleFile)
+import Language.Oberon (parseAndResolveModule)
+import Language.Oberon.Pretty ()
+import qualified Language.Oberon.Resolver as Resolver
+
+import Prelude hiding (readFile)
 
 main = exampleTree "" "examples" >>= defaultMain . testGroup "Oberon"
+
+width = 80
+contextLines = 3
 
 exampleTree ancestry path =
    do let fullPath = combine ancestry path
@@ -18,8 +30,15 @@ exampleTree ancestry path =
          then (:[]) . testGroup path . concat <$> (listDirectory fullPath >>= mapM (exampleTree fullPath))
          else if ".Mod" `isSuffixOf` path
               then return . (:[]) . testCase path $
-                   do resolvedModule <- parseAndResolveModuleFile True fullPath
-                      case resolvedModule
-                           of Failure err -> assertFailure (show err)
-                              Success{} -> pure ()
+                   do moduleSource <- readFile fullPath
+                      prettyModule <- prettyFile ancestry moduleSource
+                      prettyModule' <- prettyFile ancestry prettyModule
+                      assertEqual "pretty" prettyModule prettyModule'
               else return []
+
+prettyFile dirPath source = do
+   resolvedModule <- parseAndResolveModule True dirPath source
+   case resolvedModule
+      of Failure (Resolver.UnparseableModule err :| []) -> assertFailure (showFailure source err contextLines)
+         Failure errs -> assertFailure (show errs)
+         Success mod -> return (renderStrict $ layoutPretty defaultLayoutOptions $ pretty mod)
