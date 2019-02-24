f87b1b18fa64cf25e0b3476fb5dfecc4784d8ac1
Mario <blamario@protonmail.com>
2019-02-24 00:45:12 -0500

    Make Inherited and Synthesized newtype

--- src/Transformation/AG.hs
+++ src/Transformation/AG.hs
@@ -5,17 +5,14 @@ module Transformation.AG where
 
 import Data.Functor.Identity
 import qualified Rank2
-import qualified Transformation as Shallow
-import qualified Transformation.Deep as Deep
-
-data Inherited a g = Inherited{inh :: Atts (Inherited a) g}
-data Synthesized a g = Synthesized{syn :: Atts (Synthesized a) g}
 
 type family Atts (f :: * -> *) x
+
+newtype Inherited a g = Inherited{inh :: Atts (Inherited a) g}
+newtype Synthesized a g = Synthesized{syn :: Atts (Synthesized a) g}
+
 deriving instance (Show (Atts (Inherited a) g)) => Show (Inherited a g)
 deriving instance (Show (Atts (Synthesized a) g)) => Show (Synthesized a g)
--- type instance Atts Identity f = f Identity
--- type instance Atts (Inherited a Rank2.~> Synthesized a) g = Atts (Inherited a) g -> Atts (Synthesized a) g
 
 type Semantics a = Inherited a Rank2.~> Synthesized a
 
@@ -32,6 +29,7 @@ knit r chSem = Rank2.Arrow knit'
 class Attribution t g local where
    attribution :: t -> local -> Rule t g
 
+-- | Drop-in implementation of 'Transformation.<$>'
 mapDefault :: (q ~ Semantics t, x ~ g q q, Rank2.Apply (g q), Attribution t g local)
            => (p x -> local) -> (p x -> x) -> t -> p x -> q x
 mapDefault getLocal getSemantics t x = knit (attribution t $ getLocal x) (getSemantics x)
